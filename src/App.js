b60a08f2bba47070ab342ae4d0ef61605f99dc72
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-01-29 13:40:41 +0200

    Implement first ver (only UI, no functionality)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {ButtonGroup, TextField} from "@mui/material";
+import {createActionButtons, createNumButtons} from "./ButtonsCreate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: "0",
+    };
+  }
+
+  render() {
+    return (
+        <div className="app">
+          <div className="output">
+            <TextField
+                id="outlined-basic"
+                // label="Calculations"
+                variant="outlined"
+                defaultValue={this.state.output}
+                className="text-field"
+            />
+          </div>
+          <div className="action-buttons">
+            <ButtonGroup
+                variant="contained"
+                aria-label="outlined primary button group"
+                // fullWidth={true}
+            >
+              {createActionButtons()}
+            </ButtonGroup>
+          </div>
+          <div className="buttons">
+            {createNumButtons()}
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
